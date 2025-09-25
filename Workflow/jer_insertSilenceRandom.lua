af431c371d517a6a3b6576eb2da0ba275d9d324f
gavion6 <vitjerabek@Vit-MacBook-Pro.local>
2025-09-25 20:59:19 +0200

    new commit

--- Workflow/jer_insertSilenceRandom.lua
+++ Workflow/jer_insertSilenceRandom.lua
@@ -1,3 +1,13 @@
+-- @description Insert silence between items with random length between min and max value
+-- @about
+-- 	#Insert Silence With Random Length
+-- @version 0.2025-25-9
+-- @author jeravi
+-- @links 
+-- @donation https://buymeacoffee.com/vitjerabek
+-- @changelog initial release
+
+
 -- Insert Silence Between Selected Items in REAPER with Randomization
 
 -- Function to create the GUI
